15553826c5b9afc4e831f94e8a5c38529ee6f165
Wikan Kuncara <wikan25@gmail.com>
2019-02-05 10:31:54 +0700

    tugas2

---
+++ tugas2/tugas2.m
@@ -0,0 +1,140 @@
+function varargout = tugas1(varargin)
+% TUGAS1 MATLAB code for tugas1.fig
+%      TUGAS1, by itself, creates a new TUGAS1 or raises the existing
+%      singleton*.
+%
+%      H = TUGAS1 returns the handle to a new TUGAS1 or the handle to
+%      the existing singleton*.
+%
+%      TUGAS1('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in TUGAS1.M with the given input arguments.
+%
+%      TUGAS1('Property','Value',...) creates a new TUGAS1 or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before tugas1_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to tugas1_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help tugas1
+
+% Last Modified by GUIDE v2.5 05-Feb-2019 09:25:18
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @tugas1_OpeningFcn, ...
+                   'gui_OutputFcn',  @tugas1_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before tugas1 is made visible.
+function tugas1_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to tugas1 (see VARARGIN)
+
+% Choose default command line output for tugas1
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes tugas1 wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+axis off;
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = tugas1_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in inputimage.
+function inputimage_Callback(hObject, eventdata, handles)
+% hObject    handle to inputimage (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+[filename pathname] = uigetfile({'*.png;*.jpg';},'File Selector');
+global image;
+image = strcat(pathname, filename);
+axes(handles.axes1);
+imshow(image);
+set(handles.edit1,'string',image);
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in clearimage.
+function clearimage_Callback(hObject, eventdata, handles)
+% hObject    handle to clearimage (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+cla(handles.axes1,'reset');
+axis off;
+
+
+% --- Executes on button press in grayscaleimage.
+function grayscaleimage_Callback(hObject, eventdata, handles)
+% hObject    handle to grayscaleimage (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global image;
+map = imread(image);
+R = map(:,:,1);
+G = map(:,:,2);
+B = map(:,:,3);
+sumR = sum(R(:));
+sumG = sum(G(:));
+sumB = sum(B(:));
+sumTotal = sum([sumR, sumG, sumB]);
+varR = sumR/sumTotal;
+varG = sumG/sumTotal;
+varB = sumB/sumTotal;
+newimage = varR * R + varG * G + varB * B;
+axes(handles.axes1);
+imshow(newimage);
